4329513b8fad9906b00c2e06e4b1f554205e2fa8
jricks86 <jricks86@gmail.com>
2023-06-11 22:28:21 -0700

    frontend functionality

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,22 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
+import "./Modal.css"
+
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  let handleInputChange = (event) => { 
+    setInputValue(event.target.value);
+  }
+
+  const handleClick = () => {
+    setShowModal(true);
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
+  const resetInput = () => {
+    setInputValue("");
+  }
+
+  const modalMessage = `Hello, ${inputValue}`;
+
   return (
+    <>
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+            <Label for="name">Enter your name</Label>
+            <Input id="name" type="text" value={inputValue} onChange={handleInputChange} />
+          </div>
+          <div className="buttons">
+            <Button onClick={handleClick}>Click Me</Button> 
+            <Button onClick={resetInput}>Reset</Button>
+          </div>
+          {showModal && (
+            <ModalComponent message={modalMessage} onClose={closeModal} />
+          )}
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
       </div>
-    </div>
+
+    </>
   )
 }
 
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,22 @@
 import React from "react"
 
-const ModalComponent = () => {
-  return <></>
+const ModalComponent = ({ message, onClose }) => {
+  // const modalMessage = `Hey, ${message}`;
+  
+  return (
+  <>
+
+    <div className="modal">
+      <div className="modal-content">
+          <h1>{message}</h1>
+          <body>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, ducimus.
+          </body>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  </>
+  )
 }
 
 export default ModalComponent
